b6e1becf7ff1a0306c35f26118fb727b9f999ff8
fangruidong <984314602@qq.com>
2019-04-12 11:04:44 +0800

    数据库连接配置、数据库测试类

--- icpForCitln/src/test/java/icp/icpForCitln/IcpForCitlnApplicationTests.java
+++ icpForCitln/src/test/java/icp/icpForCitln/IcpForCitlnApplicationTests.java
@@ -1,3 +1,10 @@
+/**
+ * Project Name: icpForCitln
+ * File Name: IcpForCitlnApplicationTests
+ * Package Name: icp.icpForCitln
+ * Date: 2019-04-12 上午 11:02
+ * Copyright (c) 2019, huafon Chuangxiang Co., Ltd. All Rights Reserved.
+ */
 package icp.icpForCitln;
 
 import org.junit.Test;
@@ -9,8 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class IcpForCitlnApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
