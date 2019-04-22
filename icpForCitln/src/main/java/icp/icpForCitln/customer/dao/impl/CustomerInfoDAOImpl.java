12622f625b1dace8ebbfa592156ca1f0280f71e5
guoxs <729725300@qq.com>
2019-04-22 13:28:36 +0800

    客户主数据 增 删 改 查

--- icpForCitln/src/main/java/icp/icpForCitln/customer/dao/impl/CustomerInfoDAOImpl.java
+++ icpForCitln/src/main/java/icp/icpForCitln/customer/dao/impl/CustomerInfoDAOImpl.java
@@ -7,11 +7,21 @@
  */
 package icp.icpForCitln.customer.dao.impl;
 
+import icp.icpForCitln.common.cache.UserInfoCache;
+import icp.icpForCitln.common.util.SessionUtil;
 import icp.icpForCitln.customer.dao.CustomerInfoDAO;
 import icp.icpForCitln.customer.eneity.CustomerInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CustomerInfoDAOImpl implements CustomerInfoDAO {
@@ -30,5 +40,131 @@ public class CustomerInfoDAOImpl implements CustomerInfoDAO {
     @Override
     public void saveTest(CustomerInfo customerInfo) {
         mongoTemplate.save(customerInfo);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:06
+     * @since: JDK 1.8
+     *
+     * @description: 保存客户信息
+     * @param: [customerInfo]
+     * @return: void
+     */
+    @Override
+    public void saveCustomerInfo(CustomerInfo customerInfo){
+        mongoTemplate.save(customerInfo);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:06
+     * @since: JDK 1.8
+     *
+     * @description: 获取用户列表
+     * @param: [pageIndex, pageSize]
+     * @return: java.util.List<icp.icpForCitln.customer.eneity.CustomerInfo>
+     */
+    @Override
+    public List<CustomerInfo> customerInfoListByPage(Integer pageIndex, Integer pageSize){
+        Pageable pageable = PageRequest.of(pageIndex,pageSize);
+
+        Query query = new Query(Criteria.where("IS_DELETE").is(2)).with(pageable).with(new Sort(Sort.Direction.DESC,"LAST_MODIFICATION_TIME"));
+
+        return mongoTemplate.find(query,CustomerInfo.class);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:06
+     * @since: JDK 1.8
+     *
+     * @description: 通过公司编码或客户名称查询公司列表
+     * @param: [pageIndex, pageSize, CodeOrName]
+     * @return: java.util.List<icp.icpForCitln.customer.eneity.CustomerInfo>
+     */
+    @Override
+    public List<CustomerInfo> customerInfoListByCodeOrName(Integer pageIndex,Integer pageSize,String CodeOrName){
+        Pageable pageable = PageRequest.of(pageIndex,pageSize);
+
+        Criteria criteria = new Criteria();
+        criteria.orOperator(Criteria.where("COMPANY_CODE").regex(CodeOrName),Criteria.where("COMPANY_NAME").regex(CodeOrName)).and("IS_DELETE").is(2);
+
+        Query query = new Query(criteria).with(pageable).with(new Sort(Sort.Direction.DESC,"LAST_MODIFICATION_TIME"));
+
+        return mongoTemplate.find(query,CustomerInfo.class);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:07
+     * @since: JDK 1.8
+     *
+     * @description: 编辑公司信息
+     * @param: [customerInfo]
+     * @return: void
+     */
+    @Override
+    public void updataCustomerInfo(CustomerInfo customerInfo) {
+        Query query = new Query(Criteria.where("_id").is(customerInfo.getId()));
+
+        Update update = new Update();
+        update.set("BANK_NAME",customerInfo.getBankName());
+        update.set("BANK_ACCOUNT",customerInfo.getBankAccount());
+        update.set("CITY",customerInfo.getCity());
+        update.set("COMPANY_ADDRESS",customerInfo.getCompanyAddress());
+        update.set("COMPANY_CODE",customerInfo.getCompanyCode());
+        update.set("COMPANY_CONTACT",customerInfo.getCompanyContact());
+        update.set("COMPANY_CONTACT_PHONE",customerInfo.getCompanyContactPhone());
+        update.set("COMPANY_EMAIL",customerInfo.getCompanyEmail());
+        update.set("COMPANY_INTRODUCTION",customerInfo.getCompanyIntroduction());
+        update.set("COMPANY_NAME",customerInfo.getCompanyName());
+        update.set("COMPANY_PICTURE",customerInfo.getCompanyPicture());
+        update.set("COMPANY_SHORT_NAME",customerInfo.getCompanyShortName());
+        update.set("COMPANY_TELEPHONE",customerInfo.getCompanyTelephone());
+        update.set("COMPANY_FAX",customerInfo.getCompanytFax());
+        update.set("COUNTRY",customerInfo.getCountry());
+        update.set("CURRENCY",customerInfo.getCurrency());
+        update.set("PROVINCE",customerInfo.getProvince());
+        update.set("UNIFIED_SOCIAL_CREDIT_CODE",customerInfo.getUnifiedSocialCreditCode());
+        update.set("LAST_MODIFIER", UserInfoCache.get(SessionUtil.getByKey("userNum")).getId());
+        update.currentDate("LAST_MODIFICATION_TIME");
+
+        mongoTemplate.upsert(query,update,CustomerInfo.class);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:07
+     * @since: JDK 1.8
+     *
+     * @description: 获取公司信息详情
+     * @param: [customerInfo]
+     * @return: icp.icpForCitln.customer.eneity.CustomerInfo
+     */
+    @Override
+    public CustomerInfo getCustomerInfoById(CustomerInfo customerInfo){
+        Query query = new Query(Criteria.where("_id").is(customerInfo.getId()));
+
+        return mongoTemplate.findOne(query,CustomerInfo.class);
+    }
+
+    /**
+     * @author: guoxs
+     * @date: 19/04/19 16:08
+     * @since: JDK 1.8
+     *
+     * @description: 删除客户
+     * @param: [customerInfo]
+     * @return: void
+     */
+    @Override
+    public void deleteCustomerInfo(CustomerInfo customerInfo){
+        Query query = new Query(Criteria.where("_id").is(customerInfo.getId()));
+
+        Update update = new Update();
+        update.set("IS_DELETE",1);
+
+        mongoTemplate.upsert(query,update,CustomerInfo.class);
     }
 }
--- icpForCitln/src/main/java/icp/icpForCitln/customer/service/CustomerService.java
+++ icpForCitln/src/main/java/icp/icpForCitln/customer/service/CustomerService.java
@@ -7,8 +7,17 @@
  */
 package icp.icpForCitln.customer.service;
 
+import icp.icpForCitln.customer.dto.CustomerInfoDto;
 import icp.icpForCitln.customer.eneity.CustomerInfo;
+import icp.icpForCitln.customer.vo.CustomerInfoVO;
+
+import java.util.List;
 
 public interface CustomerService {
     void customerInfoSaveTest(CustomerInfo customerInfo);     //存储测试
+    void saveCustomerInfo(CustomerInfoDto customerInfo);  //储存客户信息
+    List<CustomerInfoVO> customerInfoListByPage(Integer pageIndex, Integer pageSize, String CodeOrName); //根据客户编码或客户名称查询客户列表
+    void updataCustomerInfo(CustomerInfoDto customerInfoDto); //修改客户信息
+    CustomerInfoVO getCustomerInfoById(CustomerInfoDto customerInfoDto);  //查询客户详情
+    void deleteCustomerInfo(CustomerInfoDto customerInfoDto); //删除客户
 }
