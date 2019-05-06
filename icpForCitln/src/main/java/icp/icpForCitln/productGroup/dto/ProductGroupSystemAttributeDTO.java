98dac7beb9ee79c930bc7ffd18a413ddb764e28a
fangruidong <984314602@qq.com>
2019-05-06 19:04:53 +0800

    新建产品 产品组属性 联动
    Signed-off-by: fangruidong <984314602@qq.com>

---
+++ icpForCitln/src/main/java/icp/icpForCitln/productGroup/dto/ProductGroupSystemAttributeDTO.java
@@ -0,0 +1,15 @@
+/**
+ * Project Name: icpForCitln
+ * File Name: ProductGroupSystemAttributeDTO
+ * Package Name: icp.icpForCitln.productGroup.dto
+ * Date: 2019/5/6 17:17
+ * Copyright (c) 2019, huafon Chuangxiang Co., Ltd. All Rights Reserved.
+ */
+package icp.icpForCitln.productGroup.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductGroupSystemAttributeDTO {   //新建产品 产品组属性查询条件
+    private String id;  //ID
+}
---
+++ icpForCitln/src/main/java/icp/icpForCitln/sysconf/dto/SystemProductAttribuitValueDTO.java
@@ -0,0 +1,17 @@
+/**
+ * Project Name: icpForCitln
+ * File Name: SystemProductAttribuitValueDTO
+ * Package Name: icp.icpForCitln.sysconf.dto
+ * Date: 2019/5/6 18:54
+ * Copyright (c) 2019, huafon Chuangxiang Co., Ltd. All Rights Reserved.
+ */
+package icp.icpForCitln.sysconf.dto;
+
+import lombok.Data;
+
+@Data
+public class SystemProductAttribuitValueDTO {
+    private String id;  //ID
+
+    private String systemProductAttribuitContent;  //产品属性值内容
+}
