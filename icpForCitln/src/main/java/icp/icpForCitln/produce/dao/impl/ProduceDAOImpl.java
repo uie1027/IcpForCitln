18d3437159e4bade53a0b0c8bbe981962fa99445
guoxs <729725300@qq.com>
2019-05-06 14:45:51 +0800

    生产入库列表 带查询

--- icpForCitln/src/main/java/icp/icpForCitln/produce/dao/impl/ProduceDAOImpl.java
+++ icpForCitln/src/main/java/icp/icpForCitln/produce/dao/impl/ProduceDAOImpl.java
@@ -14,9 +14,12 @@ import icp.icpForCitln.produce.dto.ProductionReceiptListDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.LookupOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,8 +30,19 @@ public class ProduceDAOImpl implements ProduceDAO {
     public List<ProductionReceiptListDTO> productionReceiptListFind(Integer pageIndex, Integer pageSize, ProductionReceiptFindDTO productionReceiptFindDTO){
         LookupOperation factory = LookupOperation.newLookup().from("FACTORY_INFO").localField("FACTORY_ID").foreignField("_id").as("FACTORY");
         LookupOperation order = LookupOperation.newLookup().from("PRODUCTION_ORDER").localField("PRODUCTION_ORDER_ID").foreignField("_id").as("ORDER");
-//        AggregationOperation match = Aggregation.match(Criteria.where("FACTORY.FACTORY_CODE").regex(productionReceiptFindDTO.getFactoryCode()).orOperator(Criteria.where("FACTORY.FACTORY_NAME").regex(productionReceiptFindDTO.getFactoryCode())).orOperator(Criteria.where("ORDER.PRODUCTION_RECEIPT_CODE").regex(productionReceiptFindDTO.getFactoryCode())));
-        Aggregation aggregation = Aggregation.newAggregation(factory,order,Aggregation.skip(pageIndex),Aggregation.limit(pageSize));
+       List<Criteria> list = new ArrayList<>();
+       Criteria criteria = new Criteria();
+        if (productionReceiptFindDTO.getFactoryCode()!=null){
+            list.add(Criteria.where("FACTORY.FACTORY_CODE").regex(productionReceiptFindDTO.getFactoryCode()));
+        }
+        if (productionReceiptFindDTO.getFactoryName()!=null){
+            list.add(Criteria.where("FACTORY.FACTORY_NAME").regex(productionReceiptFindDTO.getFactoryName()));
+        }
+        if (productionReceiptFindDTO.getProductionReceiptCode()!=null){
+            list.add(Criteria.where("PRODUCTION_RECEIPT_CODE").regex(productionReceiptFindDTO.getProductionReceiptCode()));
+        }
+        AggregationOperation match = Aggregation.match(criteria.orOperator(list.toArray(new Criteria[list.size()])));
+        Aggregation aggregation = Aggregation.newAggregation(factory,order,match,Aggregation.skip(pageIndex),Aggregation.limit(pageSize));
         return mongoTemplate.aggregate(aggregation,"PRODUCTION_RECEIPT",ProductionReceiptListDTO.class).getMappedResults();
     }
 }
