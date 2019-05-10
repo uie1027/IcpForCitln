74fc80e7b8700f0b6168ed2d8840cb90e85e0802
guoxs <729725300@qq.com>
2019-05-10 17:00:53 +0800

    bugfix

--- icpForCitln/src/main/java/icp/icpForCitln/produce/dao/ProduceDAO.java
+++ icpForCitln/src/main/java/icp/icpForCitln/produce/dao/ProduceDAO.java
@@ -1,10 +1,5 @@
 package icp.icpForCitln.produce.dao;
 
-import icp.icpForCitln.stock.dto.ProductionReceiptFindDTO;
-import icp.icpForCitln.stock.dto.ProductionReceiptListDTO;
-
-import java.util.List;
-
 public interface ProduceDAO {
 
 }
